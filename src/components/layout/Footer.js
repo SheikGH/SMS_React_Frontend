aa378e7d29f5c5ad66b866a0fa436c353aab919a
SheikGH <sheik.ak2006@gmail.com>
2025-05-17 22:02:11 +0400

    Code Change & User Guide

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="py-1 bg-dark navbar">
             <h1 style={{border:'solid 1px grey'}}></h1>
             <p className="text-center text-white mt-1" style={{fontSize:'25px'}}>
-                Customer Management System - 2023-2024, All Rights Reserved
+                Student Management System - 2023-2024, All Rights Reserved
             </p>
         </footer>
     )
